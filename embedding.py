16c322636be6b29c9e4b505a7901d4730e83517b
LBK <libokunhmh@gmail.com>
2021-12-29 06:49:36 +0000

    1

--- embedding.py
+++ embedding.py
@@ -48,4 +48,78 @@ class Embedding(metaclass=SingletonMetaclass):
         data = pd.read_csv(path, sep='\t')
         data = data.fillna("")
 
-        data["text"] = data['text'].apply(lambda )
+        data["text"] = data['text'].apply(lambda x: " ".join([w for w in x.split()
+                                                              if w not in self.stopwords and w != '']))
+        self.labelToIndex = label2idx(data)
+        data["label"] = data['label'].map(self.labelToIndex)
+        data["label"] = data.apply(lambda row: float(row['label']), axis=1)
+        data = data[['text', 'label']]
+
+        self.train = data['text'].tolist()
+
+    def trainer(self):
+        """
+        @description: 训练 tfidf, word2vec, fasttext 喝 autoencoder
+        :return:
+        """
+        count_vect = TfidfVectorizer(stop_words=self.stopwords,
+                                     max_df=0.4,
+                                     min_df=0.001,
+                                     ngram_range=(1, 2))
+
+        print(self.train[:5])
+        self.tfidf = count_vect.fit(self.train)
+
+        self.train = [sample.split() for sample in self.train]
+        self.w2v = models.Word2Vec(min_count=2,
+                                   window=5,
+                                   vector_size=300,
+                                   sample=6e-5,
+                                   alpha=0.03,
+                                   min_alpha=0.0007,
+                                   negative=15,
+                                   workers=4,
+                                   epochs=30,
+                                   max_vocab_size=50000)
+        self.w2v.build_vocab(self.train)
+        self.w2v.train(self.train,
+                       total_examples=self.w2v.corpus_count,
+                       epochs=15,
+                       report_delay=1)
+
+        self.id2word = gensim.corpora.Dictionary(self.train)
+        corpus = [self.id2word.doc2bow(text) for text in self.train]
+        self.LDAmodel = LdaMulticore(corpus=corpus,
+                                     id2word=self.id2word,
+                                     num_topics=30,
+                                     workers=4,
+                                     chunksize=4000,
+                                     passes=7,
+                                     alpha="asymmetric")
+
+    def saver(self):
+        """
+        保存所有模型
+        :return:
+        """
+        joblib.dump(self.tfidf, "./model/tfidf")
+
+        self.w2v.wv.save_word2vec_format("./model/w2v.bin", binary=False)
+
+        self.LDAmodel.save("./model/lda")
+
+    def load(self):
+        """
+        加载所有Embedding模型
+        :return:
+        """
+        self.tfidf = joblib.load("./model/tfidf")
+        self.w2v = models.KeyedVectors.load_word2vec_format("./model/w2v.bin", binary=False)
+        self.lda = models.ldamodel.LdaModel.load("./model/lda")
+
+
+if __name__=="__main__":
+    em = Embedding()
+    em.load_data(config.train_data_file)
+    em.trainer()
+    em.saver()
